45a16039716e57298c1910e5893597304356f118
Yoshikazu Kamimura <yoshikazu.kamimura@gmail.com>
2021-03-10 14:25:04 +0000

    second commit

--- slot.rb
+++ slot.rb
@@ -4,7 +4,6 @@ puts "ポイント　0"
 coin = 100
 point = 0
 loop do
-s
  select = [5,10,20,"やめとく"]
   num1 = rand(9)
   num2 = rand(9)
@@ -43,23 +42,23 @@ s
  puts "|#{num2}|#{num5}|#{num8}|"
  puts "|#{num3}|#{num6}|#{num9}|"
 
- if (num2 == num5) || ( num5 == num8 ) || (num8 == 7)
+ if (num2 == num5) && ( num5 == num8 ) && (num8 == 7)
  puts "-------------"
  puts "おめでとう！真ん中に7が揃いました！"
  puts "100コイン獲得！"
     coin = coin + 100 + 100 - select[putin]
- puts "残りのコイン数：#{coin}"　#この行でコインの増減を表示したい
- elsif (num2 == num5) || (num5 == num8) 
+ puts "残りのコイン数 #{coin}" #この行でコインの増減を表示したい
+ elsif (num2 == num5) || (num5 == num8)
  puts "-------------"
  puts "おめでとう！真ん中が同じ数字になりました！"
  puts "50コイン獲得！"
     coin = coin + 100 + 50 - select[putin]
- puts "残りのコイン数：#{coin + 50 - select[putin]}"　#この行でコインの増減を表示したい
+ puts "残りのコイン数：#{coin + 50 - select[putin]}" #この行でコインの増減を表示したい
  else
  puts "-------------"
  puts "残念！もう一回挑戦しよう！"
     coin = coin + 100 - select[putin]
- puts "残りのコイン数：#{coin - select[putin]}"　#この行でコインの増減を表示したい
+ puts "残りのコイン数：#{coin - select[putin]}" #この行でコインの増減を表示したい
  end
 
 end
